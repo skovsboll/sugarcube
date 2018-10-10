fbaad7c03a6d043c5e710c253429d1320918e7bf
Søren Skovsbøll <soren@wero.es>
2018-10-10 16:22:46 +0200

    support any screen resolution and scale, now and in the future

--- lib/ios/sugarcube-568/uiimage.rb
+++ lib/ios/sugarcube-568/uiimage.rb
@@ -1,25 +1,10 @@
 class UIImage
   class << self
-    SugarCube568_TallSuffix = '-568h@2x'  # 320 x 568
-    SugarCube667_TallSuffix = '-667h@2x'  # 375 x 667
-    SugarCube736_TallSuffix = '-736h@2x'  # 414 x 736
-    SugarCube1024_TallSuffix = '-1024h@2x'  # 768 x 1024
-
     def sugarcube568_imageNamed(name)
-      case UIScreen.mainScreen.bounds.size.height
-      when 568
-        taller_image_ext = SugarCube568_TallSuffix
-      when 667
-        taller_image_ext = SugarCube667_TallSuffix
-      when 736
-        taller_image_ext = SugarCube736_TallSuffix
-      when 1024
-        taller_image_ext = SugarCube1024_TallSuffix
-      else
-        taller_image_ext = false
-      end
+      main_screen = UIScreen.mainScreen
+      taller_image_ext = "-#{main_screen.bounds.size.height.to_i}@#{main_screen.scale.to_i}x"
 
-      if ( taller_image_ext && name.length > 0 && name.rangeOfString(taller_image_ext).location == NSNotFound )
+      if (taller_image_ext && name.length > 0 && name.rangeOfString(taller_image_ext).location == NSNotFound)
         # Check if is there a path extension or not
         test_name = name
         if test_name.pathExtension.length > 0
